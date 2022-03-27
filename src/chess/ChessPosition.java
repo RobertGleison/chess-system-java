c1bfe9231bfcd5649653e284b49949b5d97af741
Robert Gleison <robertgleison12@gmail.com>
2022-03-27 11:10:23 -0300

    Moving pieces

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosition {
 	private char column;
 	private int row;
@@ -20,6 +22,18 @@ public class ChessPosition {
 		return row;
 	}
 	
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+
+	@Override
+	public String toString() {
+		return "" + column + row;
 	 
 	
+}
 }
